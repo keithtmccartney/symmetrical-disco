ea5843e639435d4a7dada0909f80ef9c4d7b1e78
Keith <keithtmccartney@hotmail.com>
2018-11-02 23:13:35 +0000

    Applying struct tags for controlling idiomatic JSON (uppercase labelling).

--- main.go
+++ main.go
@@ -12,9 +12,9 @@ import (
 
 // Todo creates a basic model
 type Todo struct {
-	Name      string
-	Completed bool
-	Due       time.Time
+	Name      string    `json:"name"`
+	Completed bool      `json:"completed"`
+	Due       time.Time `json:"due"`
 }
 
 // Todos is a slice (an ordered collection) of the Todo model
