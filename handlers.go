903f1ef3c3e529c43df188c9cd181f2e07621519
Keith <keithtmccartney@hotmail.com>
2018-11-04 19:52:16 +0000

    Applying handler changes to open up the body of the request (io.LimitReader is a good way to protect against malicious attacks on the server, imagine if someone wanted to send 500GBs of JSON!); after reading the body Unmarshal is applied to the Todo struct (if that fails changes have been added to not only respond with the appropriate status code - 422 - but sending of the error in a JSON string is included (this allows the client to understand not only that something went wrong but that the ability to communicate specifically what went wrong is included)).
    Finally, sending back the status code - 201 - means that the entity was successfully created, alongside this includes the sending back of the JSON representation of the entity that was created as it contains an 'id' that the client will likely need.

--- handlers.go
+++ handlers.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -35,4 +37,37 @@ func TodoShow(w http.ResponseWriter, r *http.Request) {
 	todoID := vars["todoID"]
 
 	fmt.Fprintln(w, "Todo show:", todoID)
+}
+
+// TodoCreate creates on the todos route
+func TodoCreate(w http.ResponseWriter, r *http.Request) {
+	var todo Todo
+
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048756))
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err := r.Body.Close(); err != nil {
+		panic(err)
+	}
+
+	if err := json.Unmarshal(body, &todo); err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(422) // unprocessable entity
+
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
+	}
+
+	t := RepoCreateTodo(todo)
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusCreated)
+
+	if err := json.NewEncoder(w).Encode(t); err != nil {
+		panic(err)
+	}
 }
